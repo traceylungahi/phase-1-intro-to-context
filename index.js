c26b7aad56aa97cb98a28f24c97f25e5d513abfd
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-17 15:12:01 +0300

    commit tree dc67c3cb9558cb7e15188f3fafc60f5fab8eb294

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function createEmployeeRecord(recordsArray) {
+    let testEmployee = {
+        firstName: recordsArray[0],
+        familyName: recordsArray[1],
+        title: recordsArray[2],
+        payPerHour: recordsArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return testEmployee
+}
+
+function createEmployeeRecords(recordArray) {
+    const recordsArray = []
+    recordArray.map(resp => {
+        recordsArray.push(createEmployeeRecord(resp))
+    })
+    return recordsArray
+}
